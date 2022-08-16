6a6c5de9b0823cfd6f7c30cfd30801ddcd36deb0
Brandon Grant <brandon.grant@thedreamcorporation.com>
2022-08-16 15:36:27 +0100

    Bump version

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 Package.describe({
-  name: 'kschingiz:meteor-elastic-apm',
-  version: '2.5.1',
+  name: 'thedreamcorporation:meteor-elastic-apm',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Performance monitoring for Meteor based on Elastic APM',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Meteor-Community-Packages/meteor-elastic-apm',
+  git: 'https://github.com/thedreamcorporation/meteor-elastic-apm',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -19,8 +19,8 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('2.4');
 
-  api.use('kschingiz:meteor-measured@1.0.3');
-  api.imply('kschingiz:meteor-measured');
+  api.use('thedreamcorporation:meteor-measured@1.0.3');
+  api.imply('thedreamcorporation:meteor-measured');
 
   api.use([
     'ecmascript',
@@ -38,6 +38,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('kschingiz:meteor-elastic-apm');
+  api.use('thedreamcorporation:meteor-elastic-apm');
   api.mainModule('meteor-elastic-apm-tests.js');
 });
